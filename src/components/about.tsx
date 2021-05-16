5f42fd307ded8a12e2d099b05409a09c82cfb8ff
KunSheng <tan_kun_sheng@tech.gov.sg>
2021-05-16 18:25:02 +0800

    Add tailwind css config, typescript config

---
+++ src/components/about.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { RouteComponentProps } from "@reach/router";
+
+export default ( props: RouteComponentProps) => {
+  return (
+    <div className="bg-white dark:bg-gray-800 flex relative z-20 items-center">
+      about
+    </div>
+  );
+};
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  important: true,
+  // Active dark mode on class basis
+  darkMode: "class",
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      backgroundImage: (theme) => ({
+        check: "url('/icons/check.svg')",
+        landscape: "url('/images/landscape/2.jpg')",
+      }),
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["checked"],
+      borderColor: ["checked"],
+      inset: ["checked"],
+      zIndex: ["hover", "active"],
+    },
+  },
+  plugins: [],
+  future: {
+    purgeLayersByDefault: true,
+  },
+};
