9722ca441fd83b2d851b6d10ba7faf0106cec259
KunSheng <tan_kun_sheng@tech.gov.sg>
2021-05-17 03:53:21 +0800

    Add whitelist to dev static site origin

--- serverless/api/job.js
+++ serverless/api/job.js
@@ -1,13 +1,14 @@
 const submit = (event, context, callback) => {
+  const whitelistedOrigin = [
+    "http://localhost:8000",
+    "http://dev-static-bickup.s3-website-ap-southeast-1.amazonaws.com",
+  ].find((whitelistOrigin) => whitelistOrigin === event.headers.origin);
   const response = {
     statusCode: 200,
     headers: {
       //   1. https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
       //   2. https://stackoverflow.com/questions/24687313/what-exactly-does-the-access-control-allow-credentials-header-do#:~:text=In%20order%20to%20reduce%20the,to%20include%20cookies%20on%20requests.&text=Responding%20with%20this%20header%20to,included%20on%20cross%2Dorigin%20requests.
-      "Access-Control-Allow-Origin":
-        event.headers.origin === "http://localhost:8000"
-          ? event.headers.origin
-          : "", // Required for CORS support to work
+      "Access-Control-Allow-Origin": whitelistedOrigin ?? "", // Required for CORS support to work
       "Access-Control-Allow-Credentials": true, // Required for CORS support to work
     },
     body: JSON.stringify({
