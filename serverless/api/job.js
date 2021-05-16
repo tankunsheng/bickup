731614ae5aa2946cba48c5c7bc83c09b1e2f0648
KunSheng <tan_kun_sheng@tech.gov.sg>
2021-05-17 03:40:25 +0800

    Test not whitelisting cors origin to see if api call still works

--- serverless/api/job.js
+++ serverless/api/job.js
@@ -5,7 +5,7 @@ const submit = (event, context, callback) => {
       //   1. https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
       //   2. https://stackoverflow.com/questions/24687313/what-exactly-does-the-access-control-allow-credentials-header-do#:~:text=In%20order%20to%20reduce%20the,to%20include%20cookies%20on%20requests.&text=Responding%20with%20this%20header%20to,included%20on%20cross%2Dorigin%20requests.
       "Access-Control-Allow-Origin":
-        event.headers.origin === "http://localhost:8000"
+        event.headers.origin === "http://localhost:8001"
           ? event.headers.origin
           : "", // Required for CORS support to work
       "Access-Control-Allow-Credentials": true, // Required for CORS support to work
