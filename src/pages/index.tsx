1909033e892880156110dbe3167b5349ef6a6e6f
KunSheng <tan_kun_sheng@tech.gov.sg>
2021-05-17 00:04:07 +0800

    Move serverless into its own dir

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { StaticImage } from "gatsby-plugin-image";
 import PageLayout from "../components/PageLayout";
 
 const IndexPage = () => {
@@ -12,10 +12,7 @@ const IndexPage = () => {
               Bick-up
             </h1>
             <h2 className="font-light max-w-2xl mx-auto w-full text-xl dark:text-white text-gray-500 text-center py-8">
-              Next.js gives you the best developer experience with all the
-              features you need for production: hybrid static &amp; server
-              rendering, TypeScript support, smart bundling, route pre-fetching,
-              and more. No config needed.
+             Bicycle Pick up
             </h2>
             <div className="flex items-center justify-center mt-4">
               <a
@@ -26,17 +23,28 @@ const IndexPage = () => {
               </a>
             </div>
           </div>
-          <div className="block w-full mx-auto mt-6 md:mt-0 relative">
-            <img
-              src="/images/object/12.svg"
-              className="max-w-xs md:max-w-2xl m-auto"
-            />
+          <div className="flex justify-center mt-12">
+            <StaticImage src="../images/icon.png" alt="A dinosaur" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-48 w-48"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+              />
+            </svg>
           </div>
         </div>
       </div>
     );
   };
-  return ( <PageLayout content={<Index />} />);
+  return <PageLayout content={<Index />} />;
 };
 
 export default IndexPage;
