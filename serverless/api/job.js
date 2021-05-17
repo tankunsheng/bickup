4a1f6037386bd6b050d1c970173f4db13399c351
KunSheng <tan_kun_sheng@tech.gov.sg>
2021-05-17 15:40:28 +0800

    Handle responses in a common fn and check cors origin whitelist

--- serverless/api/job.js
+++ serverless/api/job.js
@@ -1,18 +1,6 @@
-const submit = (event, context, callback) => {
-  const whitelistedOrigin = [
-    "http://localhost:8000",
-    "http://dev-static-bickup.s3-website-ap-southeast-1.amazonaws.com",
-  ].find((whitelistOrigin) => whitelistOrigin === event.headers.origin);
-  console.log(whitelistedOrigin ?? "nothing")
-  console.log(whitelistedOrigin)
+const { handleResponse } = require("./lib/helper");
+const submit = async (event, context) => {
   const response = {
-    statusCode: 200,
-    headers: {
-      //   1. https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-      //   2. https://stackoverflow.com/questions/24687313/what-exactly-does-the-access-control-allow-credentials-header-do#:~:text=In%20order%20to%20reduce%20the,to%20include%20cookies%20on%20requests.&text=Responding%20with%20this%20header%20to,included%20on%20cross%2Dorigin%20requests.
-      "Access-Control-Allow-Origin": whitelistedOrigin ?? "", // Required for CORS support to work
-      "Access-Control-Allow-Credentials": true, // Required for CORS support to work
-    },
     body: JSON.stringify({
       message: "Go Serverless v1.0! Your function executed successfully!",
       input: event,
@@ -20,7 +8,7 @@ const submit = (event, context, callback) => {
   };
   console.log(event);
   console.log(context);
-  callback(null, response);
+  return handleResponse(event, response);
 };
 module.exports = {
   submit,
---
+++ serverless/api/lib/helper.js
@@ -0,0 +1,28 @@
+const whitelist = [
+  "http://localhost:8000",
+  "http://dev-static-bickup.s3-website-ap-southeast-1.amazonaws.com",
+];
+const checkIfOriginIsCorsWhitelisted = (whitelist, origin) => {
+  return whitelist.find(
+    (whitelistOrigin) => whitelistOrigin === origin
+  );
+};
+const handleResponse = (request, response) => {
+  const whitelistOrigin = checkIfOriginIsCorsWhitelisted(
+    whitelist,
+    request.headers.origin
+  );
+  return {
+    ...response,
+    statusCode: 200,
+    headers: {
+      //   1. https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+      //   2. https://stackoverflow.com/questions/24687313/what-exactly-does-the-access-control-allow-credentials-header-do#:~:text=In%20order%20to%20reduce%20the,to%20include%20cookies%20on%20requests.&text=Responding%20with%20this%20header%20to,included%20on%20cross%2Dorigin%20requests.
+      "Access-Control-Allow-Origin": whitelistOrigin ?? "", // Required for CORS support to work
+      "Access-Control-Allow-Credentials": true, // Required for CORS support to work
+    },
+  };
+};
+module.exports = {
+  handleResponse,
+};
