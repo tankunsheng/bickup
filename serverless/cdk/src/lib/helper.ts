f06c10e14c48a5b8fc1f56e9d2ec036452ff9c3f
KunSheng <tan_kun_sheng@tech.gov.sg>
2021-07-03 21:52:18 +0800

    debug cors headers

--- serverless/cdk/src/lib/helper.ts
+++ serverless/cdk/src/lib/helper.ts
@@ -25,7 +25,7 @@ const handleResponse = (request: any, response: any) => {
       //   2. https://stackoverflow.com/questions/24687313/what-exactly-does-the-access-control-allow-credentials-header-do#:~:text=In%20order%20to%20reduce%20the,to%20include%20cookies%20on%20requests.&text=Responding%20with%20this%20header%20to,included%20on%20cross%2Dorigin%20requests.
       // https://docs.aws.amazon.com/apigateway/latest/developerguide/how-to-cors.html
       "Access-Control-Allow-Origin": whitelistOrigin ?? "", // Required for CORS support to work
-      // "Access-Control-Allow-Credentials": true, // Required for CORS support to work
+      "Access-Control-Allow-Credentials": true, // Required for CORS support to work
       "Access-Control-Allow-Headers": "Authorizer"
     },
   };
