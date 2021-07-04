044debb9af7953cd2e2e26b6192b4b3b0d878da0
KunSheng <tan_kun_sheng@tech.gov.sg>
2021-07-05 00:42:52 +0800

    debug

--- serverless/cdk/lib/backend-stack.ts
+++ serverless/cdk/lib/backend-stack.ts
@@ -243,7 +243,7 @@ export class BackendStack extends cdk.Stack {
     const patchJobFn = new NodejsFunction(this, "bickup-patchjob-fn", {
       functionName: `${config.deploymentEnv}-bickup-patchjob-fn`,
       runtime: Runtime.NODEJS_14_X,
-      entry: "./build/jobs.js",
+      entry: "./build/src/jobs.js",
       handler: "patchJob",
       role: rwJobsTableLambdaRole,
       environment: {
